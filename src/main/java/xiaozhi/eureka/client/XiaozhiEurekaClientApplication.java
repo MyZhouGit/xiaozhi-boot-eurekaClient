1aeb6c71d4676759551b555c756c07b2ac2ac618
MyZhouGit <18312696009@163.com>
2019-04-15 14:19:09 +0800

    Initial commit

---
+++ src/main/java/xiaozhi/eureka/client/XiaozhiEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package xiaozhi.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class XiaozhiEurekaClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XiaozhiEurekaClientApplication.class, args);
+  }
+}
